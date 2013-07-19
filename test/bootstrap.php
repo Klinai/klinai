ee3088c9f9296e757d50b7e19694d5202ebcd877
unknown <c.sahs@CLEMENS.franchise.local>
2013-07-19 16:25:52 +0200

    clean bootstrap

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,21 +1,2 @@
 <?php
 
-defined('KLINAI_LIB_PATH') || define('KLINAI_LIB_PATH', __DIR__ . '../library/Klinai/');
-
-function klinaiAutoload ($className) {
-    if ( !preg_match('#^(?<major_ns>Klinai)\(?<className>(.*)$#', $className,$matches) ) {
-        return false;
-    }
-
-    $classPath = str_replace('\\', DIRECTORY_SEPARATOR, $matches['className']);
-
-    $filePath = KLINAI_LIB_PATH . $classPath;
-    if ( !file_exists($filePath) ) {
-        return false;
-    }
-    require_once $filePath;
-
-    return class_exists($className);
-}
-
-spl_autoload_register(klinaiAutoload);
