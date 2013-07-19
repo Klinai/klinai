a28efab92d96df66e5c0f9fd40dd819c6ac61efa
unknown <c.sahs@CLEMENS.franchise.local>
2013-07-19 15:20:05 +0200

    create tests

---
+++ test/KlinaiTest/Document.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace KlinaiTest;
+
+
+class DocumentTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreateDocument()
+    {
+        $this->assertTrue(FALSE);
+    }
+    public function testGetDocument()
+    {
+        $this->assertTrue(FALSE);
+    }
+    public function testUpdateDocument()
+    {
+        $this->assertTrue(FALSE);
+    }
+
+}
