6d27fc2293029575164c7ef087421f2590436f9b
unknown <c.sahs@CLEMENS.franchise.local>
2013-07-22 13:14:14 +0200

    include autoloading in UnitTests

--- tests/_autoload.php
+++ tests/_autoload.php
@@ -1,3 +1,6 @@
 <?php
 
-include_once __DIR__ . '/../vendor/autoload.php';
+/* @var $loader Composer\Autoload\ClassLoader */
+$loader = include_once __DIR__ . '/../vendor/autoload.php';
+
+$loader->set('KlinaiTest', __DIR__ . '/KlinaiTest/' );
