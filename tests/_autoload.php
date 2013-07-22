c53b10a7b9a70e835e1f7732e379908b255a8a9b
unknown <c.sahs@CLEMENS.franchise.local>
2013-07-22 14:36:34 +0200

    update autoload reset bootstrap file

--- tests/_autoload.php
+++ tests/_autoload.php
@@ -1,6 +1,23 @@
 <?php
 
-/* @var $loader Composer\Autoload\ClassLoader */
-$loader = require __DIR__ . '/../vendor/autoload.php';
 
-$loader->set('KlinaiTest', __DIR__ . '/KlinaiTest/' );
+$loader = __DIR__ . '/../vendor/autoload.php';
+
+// local testing
+if (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
+    $loader = __DIR__ . '/../../../../vendor/autoload.php';
+}
+
+if (!$loader = @include($loader)) {
+    echo <<<EOM
+You must set up the project dependencies by running the following commands:
+
+    curl -s http://getcomposer.org/installer | php
+    php composer.phar install
+
+EOM;
+
+    exit(1);
+}
+
+$loader->add('KlinaiTest', __DIR__ );
