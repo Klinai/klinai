7cceea7d890f79fba20ba55cb6b649c2a4d744db
Clemens Sahs <clemens.sahs@slides-worker.org>
2013-07-23 12:59:43 +0200

    add test for ClientTest - testUpdateDoc - testGetDocument

--- tests/KlinaiTest/Client/ClientTest.php
+++ tests/KlinaiTest/Client/ClientTest.php
@@ -38,9 +38,50 @@ class ClientTest extends \PHPUnit_Framework_TestCase
 
     }
 
-    public function testGetDoc()
+    public function testUpdateDoc()
     {
-        $this->markTestIncomplete();
+        $docData1 = array(
+            'key1'=>'foo',
+            'key2'=>'bar',
+        );
+
+        $response1 = $this->client->storeDoc('client_test1', $docData1);
+
+        $this->assertObjectHasAttribute("id", $response1);
+        $this->assertObjectHasAttribute("rev", $response1);
+
+
+        $docData2 = array(
+            '_id'=>$response->id,
+            '_rev'=>$response->rev,
+            'key1'=>'foo',
+            'key2'=>'bar',
+        );
+        $response2 = $this->client->storeDoc('client_test1', $docData2);
+
+        $this->assertObjectHasAttribute("id", $response2);
+        $this->assertObjectHasAttribute("rev", $response2);
+
+        $this->assertEquals($response1->id,$response2->id);
+        $this->assertNotEquals($response1->rev,$response2->rev);
+
+        $this->assertRegExp('/2-(.+)/',$response2->rev);
+
+    }
+
+    public function testGetDocument()
+    {
+        $docData = array(
+            'key1'=>'foo',
+            'key2'=>'bar',
+        );
+
+        $response = $this->client->storeDoc('client_test1', $docData);
+
+        $doc = $this->client->getDoc('client_test1', $response->id);
+        $fields = $doc->getFields();
+        $this->assertObjectHasAttribute("key1", $fields);
+        $this->assertObjectHasAttribute("key2", $fields);
     }
 
     public function testDatabaseIsNotExists()
