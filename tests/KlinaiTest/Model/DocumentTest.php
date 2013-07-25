7529fd8a6963e76d2099a67273c259f0c23f1f00
Clemens Sahs <clemens.sahs@slides-worker.org>
2013-07-25 14:52:36 +0200

    add tests for Document

--- tests/KlinaiTest/Model/DocumentTest.php
+++ tests/KlinaiTest/Model/DocumentTest.php
@@ -3,23 +3,158 @@
 namespace KlinaiTest\Model;
 
 use \PHPUnit_Framework_TestCase;
+use Klinai\Model\Document;
 
-class DocumentTest extends PHPUnit_Framework_TestCase {
+class DocumentTest extends PHPUnit_Framework_TestCase
+{
+    protected $mockClient;
 
+    public function setUp()
+    {
+        $this->configArray = require './tests/_files/config.php';
+        $this->mockClient = $this->getMock('Klinai\Client\Client');
+        $this->mockClient->setConfig($this->configArray);
+    }
+    public function testSetSourceDatabase()
+    {
+        $data=array(
+            'foo'=>'bar',
+            'boo'=>'bar',
+        );
+        $sourceDatabase = 'client_test2';
+
+        $doc = new Document($data, $this->mockClient,'client_test1');
+        $doc->setSourceDatabase($sourceDatabase);
+
+        $this->assertEquals($doc->getSourceDatabase(), $sourceDatabase);
+
+    }
     public function testCreateDocument()
     {
-        $this->markTestIncomplete();
+        $mockReturn = (object) array (
+            'id'=>'fooBar',
+            'rev'=>'1-4861618161879616'
+        );
+
+        $this->mockClient = $this->getMock('Klinai\Client\Client');
+        $this->mockClient->expects($this->once())
+                         ->method('storeDoc')
+                         ->will($this->returnValue($mockReturn));
+
+        $data=array(
+            'foo'=>'bar',
+            'boo'=>'bar',
+        );
+
+        $doc = new Document($data, $this->mockClient,'client_test1');
+        $doc->record();
+
+        $this->assertEquals($mockReturn->id, $doc->get('_id'));
+        $this->assertEquals($mockReturn->rev, $doc->get('_rev'));
     }
     public function testGetDocument()
     {
-        $this->markTestIncomplete();
-    }
-    public function testUpdateDocument()
-    {
-        $this->markTestIncomplete();
+        $mockReturn = array(
+            'fooBar'=> (object) array (
+                'id'=>'fooBar',
+                'rev'=>'1-4861618161879616',
+                'foo'=>'bar',
+                'boo'=>'bar',
+            ),
+            'barfoo'=> (object) array (
+                'id'=>'barfoo',
+                'rev'=>'2-4861618161879616',
+                'foo'=>'dummy',
+                'boo'=>'dummy',
+            )
+        );
+
+        $this->mockClient = $this->getMock('Klinai\Client\Client');
+        $this->mockClient->expects($this->once())
+                         ->method('getDoc')
+                         ->with($this->equalTo('client_test1'),
+                                $this->equalTo('fooBar'))
+                         ->will($this->returnValue($mockReturn['fooBar']));
+
+        $this->mockClient->expects($this->once())
+                         ->method('getDoc')
+                         ->with($this->equalTo('client_test1'),
+                                $this->equalTo('barfoo'))
+                         ->will($this->returnValue($mockReturn['barfoo']));
+
+        $doc1 = $this->mockClient->getDoc ( 'client_test1', 'fooBar' );
+        $doc2 = $this->mockClient->getDoc ( 'client_test1', 'barfoo' );
+
+        $this->assertEquals($mockReturn['fooBar']->id,  $doc1->get('_id'));
+        $this->assertEquals($mockReturn['fooBar']->rev, $doc1->get('_rev'));
+        $this->assertEquals($mockReturn['fooBar']->foo, $doc1->get('foo'));
+        $this->assertEquals($mockReturn['fooBar']->boo, $doc1->get('boo'));
+
+        $this->assertEquals($mockReturn['barfoo']->id,  $doc2->get('_id'));
+        $this->assertEquals($mockReturn['barfoo']->rev, $doc2->get('_rev'));
+        $this->assertEquals($mockReturn['barfoo']->foo, $doc2->get('foo'));
+        $this->assertEquals($mockReturn['barfoo']->boo, $doc2->get('boo'));
     }
     public function testDeleteDocument()
     {
         $this->markTestIncomplete();
+    }
+    public function testIsAttachmentExists()
+    {
+        $mockReturn = json_decode('{
+           "_id": "fooBar",
+           "_rev": "1-7182e53ce7cd148307e40521e9ede288",
+           "foo" : "dummy",
+           "boo" : "dummy",
+           "_attachments": {
+               "attachment1": {
+                   "content_type": "image/jpeg",
+                   "revpos": 11,
+                   "digest": "md5-ezhD8uRb10w4JpIqaH8R+A==",
+                   "length": 125664,
+                   "stub": true
+               },
+           }
+        }',false);
+
+        $this->mockClient = $this->getMock('Klinai\Client\Client');
+        $this->mockClient->expects($this->once())
+                         ->method('getDoc')
+                         ->will($this->returnValue($mockReturn));
+
+        /* @var $doc \Klinai\Model\Document */
+        $doc = $this->mockClient->getDoc ( 'client_test1', 'fooBar' );
+
+        $this->assertTrue($doc->isAttachmentExists('attachment1'));
+        $this->assertFalse($doc->isAttachmentExists('notExists'));
+
+    }
+    public function testGetAttachment()
+    {
+        $mockReturn = json_decode('{
+           "_id": "fooBar",
+           "_rev": "1-7182e53ce7cd148307e40521e9ede288",
+           "foo" : "dummy",
+           "boo" : "dummy",
+           "_attachments": {
+               "attachment1": {
+                   "content_type": "image/jpeg",
+                   "revpos": 11,
+                   "digest": "md5-ezhD8uRb10w4JpIqaH8R+A==",
+                   "length": 125664,
+                   "stub": true
+               },
+           }
+        }',false);
+
+        $this->mockClient = $this->getMock('Klinai\Client\Client');
+        $this->mockClient->expects($this->once())
+                         ->method('getDoc')
+                         ->will($this->returnValue($mockReturn));
+
+        /* @var $doc \Klinai\Model\Document */
+        $doc = $this->mockClient->getDoc ( 'client_test1', 'fooBar' );
+
+        $this->assertInstanceOf($doc->getAttachment('attachment1'),'Klinai\\Model\\Attchment');
     }
 }
