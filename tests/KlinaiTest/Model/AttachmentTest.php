5adba76167e1e52488635f20390be75429aeeb40
Clemens Sahs <clemens.sahs@slides-worker.org>
2013-08-27 16:28:11 +0200

    add attachment tests

---
+++ tests/KlinaiTest/Model/AttachmentTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace KlinaiTest\Model;
+
+use \PHPUnit_Framework_TestCase;
+use Klinai\Model\Document;
+use Klinai\Client\ClientConfig;
+
+class AttachmentTest extends PHPUnit_Framework_TestCase
+{
+
+}
